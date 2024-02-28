a6fc1505c453d0afe94c712a3ebb002fee821657
poltrujillo <poltrujialb@gmail.com>
2024-02-28 12:42:21 +0100

    Create Item Model

---
+++ src/main/java/com/poltrujillo/dependencyinjection2/models/Item.java
@@ -0,0 +1,22 @@
+package com.poltrujillo.dependencyinjection2.models;
+
+public class Item {
+    private Product product;
+    private Integer quantity;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
