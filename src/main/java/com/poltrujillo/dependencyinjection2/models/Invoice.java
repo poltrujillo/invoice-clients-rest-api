9f281a848713c2b59b0e3776770e931226df8f13
poltrujillo <poltrujialb@gmail.com>
2024-02-28 12:42:14 +0100

    Create Invoice Model

---
+++ src/main/java/com/poltrujillo/dependencyinjection2/models/Invoice.java
@@ -0,0 +1,33 @@
+package com.poltrujillo.dependencyinjection2.models;
+
+import java.util.List;
+
+public class Invoice {
+    private Client client;
+    private String description;
+    private List<Item> items;
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
